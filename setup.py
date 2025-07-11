940b655ab03fd350fa158db7689dddf3ac29e29b
DeeptiShuklaProject <deeptishukla515@gmail.com>
2025-07-11 18:28:17 +0530

    added project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'medical_chatbot',
+    version= '0.0.0',
+    author= 'DeeptiShuklaProject',
+    author_email= 'deeptishukla515@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
